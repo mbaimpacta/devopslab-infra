dd007c85a733e030c398a64bf9207e901100c71f
Vagrant User <vagrant@multicloud>
2021-08-14 11:31:31 -0300

    criação app engine

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,21 +1,5 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "firstvm" {
-  name         = "helloworld"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
-
-  # Definir a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-1804-bionic-v20210720"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default"
-
-    access_config {
-    }
-  }
+#Criando um appEngine
+resource "google_app_engine_application" "app" {
+project = "devopslab_infra "
+location_id = "us-central" 
 }
