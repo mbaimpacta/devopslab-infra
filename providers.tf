151e82029212186c02b1bf0514e98c0bb9560200
mbaimpacta <80072779+mbaimpacta@users.noreply.github.com>
2021-08-14 10:04:27 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,8 +8,7 @@ terraform {
 }
 
 # Configura o Provider Google Cloud com o Projeto
-provider "google" {
-  credentials = file("/home/vagrant/aula-devops-cloud-0dc060e083a0.json")
+provider "google" {  
   project     = "aula-devops-cloud"
   region      = "us-central1"
   zone        = "us-central1-c"
